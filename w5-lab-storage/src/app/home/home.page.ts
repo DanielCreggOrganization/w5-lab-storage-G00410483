e36b2fba14f2967e56523c94368109533e98630f
David Vlasic <120279522+G00410483@users.noreply.github.com>
2024-10-15 15:02:02 +0000

    Fixing the imports for this component

--- w5-lab-storage/src/app/home/home.page.ts
+++ w5-lab-storage/src/app/home/home.page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
   key: string = '';
